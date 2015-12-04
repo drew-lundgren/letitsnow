25b71462f57313c381d9f5272649abf0ab8b34d3
rumski20 <rumski20@msn.com>
2015-12-03 23:27:37 -0600

    started to parse

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$.ajaxPrefilter( function (options) {
+$.ajaxPrefilter(function (options) {
   if (options.crossDomain && jQuery.support.cors) {
     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
@@ -6,8 +6,32 @@ $.ajaxPrefilter( function (options) {
   }
 });
 
-$.get( "http://www.nws.noaa.gov/view/prodsByState.php?state=MN&prodtype=public#LSRDLH")
-.done(
-function( data ) {
-    console.log(data);
-});
+var raw;
+
+$.get("http://www.nws.noaa.gov/view/prodsByState.php?state=MN&prodtype=public#LSRDLH")
+  .done(
+    function (data) {
+      raw = data
+      //console.log(raw);
+      parse(raw);
+    });
+
+
+
+function parse(raw) {
+//  console.log(raw.indexOf('\n'));
+  var re = /(\d{4}\s[A-Z]{2})/m;
+  var sections = raw.split("<pre>"),
+      lines;
+  //console.log(sections);
+  sections.forEach( function(section) {
+    lines = section.split('\n');
+    var tabs;
+    lines.forEach( function(line) {
+      spaces = line.split(/\s/).filter( function(space) {
+        return space != "";
+      });
+      console.log(spaces);
+    });
+  });
+}
