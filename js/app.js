b373d61a69ec7c6743c37730fa4bda7d120b3b18
Kris Johnson <krisj@northpointgis.com>
2015-12-28 07:47:40 -0600

    re-worked parsing

--- js/app.js
+++ js/app.js
@@ -54,11 +54,27 @@ function _parse(raw) {
             latLng: /\d{2}\.\d{2}[N]\s\d{2}\.\d{2}[W]/,
             value: /\d{1,2}\.\d{1,2}(?=\sINCH)/,
             date: /\d{2}\/\d{2}\/\d{4}/,
-            time: /(\d{4}\s(AM|PM))/
+            time: /(\d{4}\s(AM|PM))/,
+            lines: /\n/
         },
         records = [],
         data = {},
-        matches = raw.split('<pre>');
+        lines = raw.split(regexs.lines),
+        matches = [];
+        lines.forEach( function(line, index, array) {
+            var match = '';
+            // check for time
+            if (regexs.time.test(line.substr(0,7))) {
+                // check where next time is
+                if (regexs.time.test(array[index+3])) {
+                    match = array.slice(index, index+3).join(' ');
+                }
+                else if (regexs.time.test(array[index+5])) {
+                    match = array.slice(index, index+5).join(' ');
+                }
+            }
+            if (match) matches.push( match );
+        });
 
     // console.log(regexs);
     console.log('matches', matches);
@@ -66,9 +82,9 @@ function _parse(raw) {
     // loop through rows
     matches.forEach( function(row) {
         // exit if it's not a weather snippet
-        if (row.substr(2, 3) != '000') {
-            return false;
-        }
+        // if (row.substr(2, 3) != '000') {
+        //     return false;
+        // }
         // set the date
         var theDate = new Date(row.match(regexs.date)[0]),
             theTime = row.match(regexs.time)[0],
